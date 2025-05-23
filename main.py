dfd0aca4f4bcb462f70f22978c2394ee1c570f9f
Shnavo <szango297@gmail.com>
2025-05-23 23:09:32 +0200

    code cleanup

--- main.py
+++ main.py
@@ -1,22 +1,23 @@
 import pygame
 from constants import *
 
-pygame.init()
 screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-def game_loop():
+
+def main():
+    pygame.init()
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
         pygame.Surface.fill(screen, (0, 0, 0))
         pygame.display.flip()
-def main():
-    print(
-        f"Starting Asteroids!\n"
-        f"Screen width: {SCREEN_WIDTH}\n"
-        f"Screen height: {SCREEN_HEIGHT}"
-        )
+
+# def main():
+#     print(
+#         f"Starting Asteroids!\n"
+#         f"Screen width: {SCREEN_WIDTH}\n"
+#         f"Screen height: {SCREEN_HEIGHT}"
+#         )
 
 if __name__ == "__main__":
     main()
-    game_loop()
